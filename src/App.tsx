8205a1bf843b1b49d3aeb4db9a7c2175a5986ee2
John Miclat <jmiclat@ufl.edu>
2023-12-18 03:28:04 -0500

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import {
+  About, Contact, Experience, Feedbacks, Hero,
+  Navbar, StarsCanvas, Tech, Works
+} from './components';
 
 const App = () => {
 
   return (
-    <>
-      <div>
-        your momma is a hoe
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+
+      <div className='bg-hero-pattern bg-cover bg-center bg-no-repeat'>
+        <Navbar />
+        <Hero/>
       </div>
-    </>
+
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+
+      <div className='relative z-0'>
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+      
+    </div>
+    </BrowserRouter>
   )
 }
 
