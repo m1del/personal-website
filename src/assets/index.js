42af0c1e38c5a5dc9e93e3f186749196aff9748a
John Miclat <jmiclat@ufl.edu>
2023-12-30 00:51:50 -0500

    Add assets to index

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import backend from "./backend.png";
+import close from "./close.svg";
 import creator from "./creator.png";
+import github from "./github.png";
+import logo from "./logo.png";
+import menu from "./menu.svg";
 import mobile from "./mobile.png";
 import web from "./web.png";
-import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -18,8 +18,8 @@ import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import typescript from "./tech/typescript.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -31,32 +31,14 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
-  logo,
-  backend,
-  creator,
-  mobile,
-  web,
-  github,
-  menu,
-  close,
-  css,
+  backend, carrent, close, creator, css,
   docker,
   figma,
-  git,
-  html,
-  javascript,
-  mongodb,
+  git, github, html,
+  javascript, jobit, logo, menu, meta, mobile, mongodb,
   nodejs,
   reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+  redux, shopify,
+  starbucks, tailwind, tesla, threejs, tripguide, typescript, web
 };
+
