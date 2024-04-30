9cc8e3e488c9b596cefef907c4fe5beb5cdcdd4b
John Miclat <miclatjohn1@gmail.com>
2024-04-30 02:13:15 -0400

    Init commit: React, Typescript, Shadcn, and Tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
