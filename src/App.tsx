612d66695ddc98b1a63fbfeac9a1999c37a2f8a8
John Miclat <miclatjohn1@gmail.com>
2024-05-01 18:31:51 -0400

    Init commit: React, Typescript, Shadcn, and Tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1>hello world</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
